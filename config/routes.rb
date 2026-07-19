1b00183d6144a9b8ef4d5d63ccd17429f41eda12
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-07-19 13:59:06 +0000

    [Add]controller,routes,edit.html更新機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
