5b201f2bf55ee3a88946f698002e152ff86a3f9e
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-07-19 13:15:12 +0000

    [Finish]3-4

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show'
+  get 'lists/:id' => 'lists#show', as: 'list'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
