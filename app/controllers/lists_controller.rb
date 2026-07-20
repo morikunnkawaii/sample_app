38a07323f77ab1952718962d5598ffae61f45803
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-07-20 08:01:06 +0000

    [Add]controller モデルとの連携

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,11 +6,14 @@ class ListsController < ApplicationController
 
   def create
     # 1.%2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+     # 3. データをデータベースに保存するためのsaveメソッド実行
+    if @list.save
+     # 4. トップ画面へリダイレクト
+      redirect_to list_path(@list.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
