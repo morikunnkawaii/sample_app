c8e5a752aea10357c1ba5ca82e11368376b12bdf
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-07-20 08:21:12 +0000

    [Add]config 修正し忘れが会ったためあった為修正 new.html viewsでエラー表示できるようにerros追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "lists/new"
   get 'top' => 'homes#top'
   resources :lists
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
